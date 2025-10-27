070bd333a2a9d868cfa64704d07154fbd0b72d2d
chr1st1annYamar1 <164941419+chr1st1annYamar1@users.noreply.github.com>
2025-10-27 23:31:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function magazine(){
+	window.location.href="pages/magazine.html";
+}
+function clubs(){
+	window.location.href="pages/clubs.html";
+}
+function updates(){
+	window.location.href="pages/updates.html"
+}
+function whatsapp(){
+	window.location.href="pages/whatsapp.html";
+}
+function instagram(){
+	window.location.href="pages/instagram.html";
+}
+function home(){
+	window.location.href="..//index.html";
+}
